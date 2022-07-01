a50cd6c4100dacf7bffb6db00a82580503491641
cam-vasquez <00065221@uca.edu.sv>
2022-06-30 19:01:13 -0600

    :card_file_box: Edit

--- DATABANKprueba.sql
+++ DATABANKprueba.sql
@@ -1,4 +1,259 @@
+CREATE DATABASE DB_BINAES_2022;
+
+DROP DATABASE DB_BINAES_2022;
+
 USE DB_BINAES_2022;
+
+--------- EVENTO ---------
+
+CREATE TABLE EVENTO(
+    id INT PRIMARY KEY,
+    titulo VARCHAR(120) NOT NULL,
+    asistentes INT NOT NULL, 
+    imagen VARBINARY(MAX) NULL, -- REVISAR SI PUEDE SER NOT NULL O NULL
+    --imagen https://docs.microsoft.com/en-us/sql/t-sql/data-types/ntext-text-and-image-transact-sql?view=sql-server-ver16
+);
+
+CREATE TABLE OBJETIVO_EVENTO(
+    id INT PRIMARY KEY,
+    objetivo VARCHAR(200) NOT NULL, 
+    id_evento INT NOT NULL,
+);
+
+--------- ACTIVIDAD  ---------
+
+CREATE TABLE ACTIVIDAD(
+    id INT PRIMARY KEY,
+    fecha_inicio DATETIME NOT NULL,
+    fecha_final DATETIME NOT NULL,
+    id_evento INT NOT NULL,
+    id_area INT NOT NULL,
+    id_usuario INT NOT NULL,
+);
+
+---------  AREA ---------
+
+CREATE TABLE AREA(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(250) NOT NULL,
+    piso INT NOT NULL,
+    id_horario INT NOT NULL,
+    id_responsable INT NOT NULL,
+    id_disponibilidad_area INT NOT NULL,
+);
+
+CREATE TABLE DISPONIBILIDAD_AREA(
+    id INT PRIMARY KEY,
+    disponible BIT NOT NULL,  
+    /* Si es 1 es true y si es 0 falsa.
+    DOCUMENTATION:
+        -https://docs.microsoft.com/en-us/sql/t-sql/data-types/bit-transact-sql?view=sql-server-ver16
+        -https://www.databasestar.com/sql-boolean-data-type/#:~:text=SQL%20Server%20Boolean,TRUE%20and%200%20for%20FALSE.
+     */
+);
+
+
+
+CREATE TABLE RESPONSABLE(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE HORARIO(
+    id INT PRIMARY KEY,
+    dias VARCHAR(50) NOT NULL,
+    horas VARCHAR(25) NOT NULL,
+);
+
+
+--------- ASISTENCIA ---------
+
+CREATE TABLE ASISTENCIA(
+    id_actividad INT NOT NULL,     
+    id_usuario INT NOT NULL,
+    fecha_entrada DATETIME NOT NULL,
+    fecha_salida DATETIME NULL,
+);
+
+--------- USUARIO ---------
+
+CREATE TABLE USUARIO(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    email VARCHAR(75) NOT NULL,
+    foto VARBINARY(MAX) NOT NULL, --* falta tratar con default
+    cod_qr VARBINARY(MAX) NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    institucion VARCHAR(125) NOT NULL,
+    ocupacion VARCHAR(100) NOT NULL,
+    id_rol_usuario INT NOT NULL,
+    id_telefono INT NOT NULL,    
+    password_user VARCHAR(120) NOT NULL,
+    --  hay un principio para tratar contraseñas en Base de datos:
+    /* DO NOT STORE PASSWORD IN A DATA BASE
+    DOCUMENTATION>
+        - https://bornsql.ca/blog/how-to-really-store-a-password-in-a-database/
+        - https://auth0.com/blog/hashing-passwords-one-way-road-to-security/
+        - https://www.mssqltips.com/sqlservertip/4037/storing-passwords-in-a-secure-way-in-a-sql-server-database/ 
+        - https://stackoverflow.com/questions/4181198/how-to-hash-a-password
+     */
+);
+
+CREATE TABLE ROL(
+    id INT PRIMARY KEY,
+    rol VARCHAR(15) NOT NULL,
+);
+
+CREATE TABLE TELEFONO(
+    id INT PRIMARY KEY,
+    telefono CHAR(12) NOT NULL UNIQUE
+        CHECK(telefono LIKE '+503[2|6|7][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+);
+
+--------- PRESTAMO Y RESERVA EJEMPLAR ---------
+
+CREATE TABLE PRESTAMO_EJEMPLAR(
+    id_usuario INT NOT NULL,
+    id_ejemplar INT NOT NULL,     
+    fecha_prestamo DATETIME NOT NULL,
+    fecha_devolucion DATETIME NOT NULL,  
+);
+
+--------- EJEMPLAR  ---------
+
+CREATE TABLE RESERVA_EJEMPLAR(
+    id_usuario INT NOT NULL,
+    id_ejemplar INT NOT NULL,     
+    fecha_reserva DATETIME NOT NULL,
+    fecha_devolucion DATETIME NOT NULL, 
+);
+
+CREATE TABLE EJEMPLAR(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    fecha_publicacion DATE NOT NULL,
+    portada VARBINARY(MAX) NOT NULL,--revisar si puede ser NOT NULL Y PONER UN DEFAULT
+    id_coleccion INT NULL,
+    id_autor INT NOT NULL,
+    id_bibliografico INT NOT NULL,
+    id_editorial INT NOT NULL,
+    id_idioma INT NOT NULL,
+    id_formato INT NOT NULL,
+    id_disponibilidad_ejemplar INT NOT NULL,
+);
+
+CREATE TABLE DISPONIBILIDAD_EJEMPLAR(
+    id INT PRIMARY KEY,
+    disponible BIT NOT NULL, 
+);
+
+CREATE TABLE AUTOR(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE ID_BIBLIOGRAFICO(
+    id INT PRIMARY KEY,
+    tipo VARCHAR(15) NULL,
+    identificador VARCHAR(50) NULL,
+);
+
+CREATE TABLE FORMATO(
+    id INT PRIMARY KEY,
+    formato VARCHAR(10) NOT NULL,
+);
+
+CREATE TABLE IDIOMA(
+    id INT PRIMARY KEY,
+    idioma VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE EDITORIAL(
+    id INT PRIMARY KEY,
+    editorial VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE PALABRA_CLAVE(
+    id INT PRIMARY KEY,
+    etiqueta VARCHAR(100) NOT NULL,
+    id_ejemplar INT NOT NULL,
+);
+
+
+--------- COLECCION ---------
+
+CREATE TABLE COLECCION(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(75) NOT NULL,
+    id_tipo INT NOT NULL,
+    id_genero INT NOT NULL,
+);
+
+CREATE TABLE GENERO(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE TIPO_COLECCION(
+    id INT PRIMARY KEY,
+    nombre VARCHAR(75) NOT NULL,
+);
+
+------------ FK's --------------
+---*
+
+ALTER TABLE OBJETIVO_EVENTO ADD CONSTRAINT fk_id_evento_objetivo FOREIGN KEY(id_evento) REFERENCES EVENTO(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+;
+
+ALTER TABLE ACTIVIDAD ADD CONSTRAINT fk_id_evento FOREIGN KEY(id_evento) REFERENCES EVENTO(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+;
+
+ALTER TABLE ACTIVIDAD ADD CONSTRAINT fk_id_area FOREIGN KEY(id_area) REFERENCES AREA(id);
+ALTER TABLE ACTIVIDAD ADD CONSTRAINT fk_id_usuario FOREIGN KEY(id_usuario) REFERENCES USUARIO(id);
+
+ALTER TABLE AREA ADD CONSTRAINT fk_id_horario FOREIGN KEY(id_horario) REFERENCES HORARIO(id);
+ALTER TABLE AREA ADD CONSTRAINT fk_id_responsable FOREIGN KEY(id_responsable) REFERENCES RESPONSABLE(id);
+
+ALTER TABLE ASISTENCIA ADD CONSTRAINT pk_asitencia  PRIMARY KEY (id_actividad, id_usuario);
+ALTER TABLE ASISTENCIA ADD CONSTRAINT fK_id_actividad_asistencia FOREIGN KEY (id_actividad) REFERENCES ACTIVIDAD(id);
+ALTER TABLE ASISTENCIA ADD CONSTRAINT fk_id_usuario_asistencia FOREIGN KEY (id_usuario) REFERENCES USUARIO(id);
+
+ALTER TABLE USUARIO ADD CONSTRAINT fk_id_rol_user FOREIGN KEY(id_rol_usuario) REFERENCES ROL(id);
+ALTER TABLE USUARIO ADD CONSTRAINT fk_id_telefono FOREIGN KEY(id_telefono) REFERENCES TELEFONO(id);
+---*
+
+ALTER TABLE PRESTAMO_EJEMPLAR ADD CONSTRAINT pk_prestamo_ejemplar PRIMARY KEY (id_usuario, id_ejemplar);
+ALTER TABLE PRESTAMO_EJEMPLAR ADD CONSTRAINT fk_id_usuario_prestamo FOREIGN KEY (id_usuario) REFERENCES USUARIO(id);
+ALTER TABLE PRESTAMO_EJEMPLAR ADD CONSTRAINT fk_id_ejemplar_prestamo FOREIGN KEY (id_ejemplar) REFERENCES EJEMPLAR(id);
+
+ALTER TABLE RESERVA_EJEMPLAR ADD CONSTRAINT pk_reserva_ejemplar PRIMARY KEY (id_usuario, id_ejemplar);
+ALTER TABLE RESERVA_EJEMPLAR ADD CONSTRAINT fk_id_usuario_reserva FOREIGN KEY (id_usuario) REFERENCES USUARIO(id);
+ALTER TABLE RESERVA_EJEMPLAR ADD CONSTRAINT fk_id_ejemplar_reserva FOREIGN KEY (id_ejemplar) REFERENCES EJEMPLAR(id);
+
+---*
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_coleccion FOREIGN KEY(id_coleccion) REFERENCES COLECCION(id)
+    ON DELETE SET NULL
+;
+
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_autor FOREIGN KEY(id_autor) REFERENCES AUTOR(id);
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_bibliografico FOREIGN KEY(id_bibliografico) REFERENCES ID_BIBLIOGRAFICO(id);
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_editorial FOREIGN KEY(id_editorial) REFERENCES EDITORIAL(id);
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_idioma FOREIGN KEY(id_idioma) REFERENCES IDIOMA(id);
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_formato FOREIGN KEY(id_formato) REFERENCES FORMATO(id);
+
+ALTER TABLE PALABRA_CLAVE ADD CONSTRAINT fk_id_ejemplar FOREIGN KEY(id_ejemplar) REFERENCES EJEMPLAR(id);
+
+--*
+ALTER TABLE COLECCION ADD CONSTRAINT fk_id_tipo FOREIGN KEY(id_tipo) REFERENCES TIPO_COLECCION(id);
+ALTER TABLE COLECCION ADD CONSTRAINT fk_id_genero FOREIGN KEY(id_genero) REFERENCES GENERO(id);
+
+ALTER TABLE AREA ADD CONSTRAINT fk_id_disponibilidad_area FOREIGN KEY (id_disponibilidad_area) REFERENCES DISPONIBILIDAD_AREA(id);
+ALTER TABLE EJEMPLAR ADD CONSTRAINT fk_id_disponibilidad_ejemplar FOREIGN KEY (id_disponibilidad_ejemplar) REFERENCES DISPONIBILIDAD_EJEMPLAR(id);
 
 ----------- INSERTS --------------------------
 
@@ -14,7 +269,6 @@ INSERT INTO EVENTO(id,titulo,asistentes,imagen) VALUES
 	(8 ,'Cinefórum: Derechos humanos y personas mayores', 76, 0x550a0385bc),
 	(9 ,'Jornada de entrenamiento en el uso de recursos online', 115, 0x5e3bb52393),
 	(10,'Exposición sobre la literatura en El Salvador', 90, 0x3d941fb9f1);
-
 INSERT INTO EVENTO(id,titulo,asistentes,imagen) VALUES 
 	(11,'Ferias de Actualización Bibliográfica'                                                                        , 65, 0x55bc3686f6),
 	(12,'Venta de Bibliografía Básica'                                                                                 , 35, 0xfcb9f108e0),
@@ -106,6 +360,8 @@ INSERT INTO EVENTO(id,titulo,asistentes,imagen) VALUES
 	(98,'Taller de Introducción a la Investigación Operativa'                                                          , 60, 0x41983dbf79),
 	(99,'Taller: La comunicación a través del juego'                                                                   , 90, 0xd8b435015f),
 	(100,'Taller para contar cuentos'                                                                                  , 122,0x0eb18a1ab1);
+
+
 
 INSERT INTO OBJETIVO_EVENTO(id,objetivo,id_evento) VALUES
     (1,'Promover la alfabetización digital y facilitar el acceso a las TICs a los miembros de la sociedad', 1),
